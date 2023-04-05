b5b206636edf80d22d155f1b9a5c6976720b019c
sfc-gh-jburns2 <joe.burns@snowflake.com>
2023-04-05 18:40:25 -0400

    update name

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "cford38"
+  default     = "jburns27"
 }
 
 variable "class_name" {
